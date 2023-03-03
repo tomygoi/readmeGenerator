31c74ca436c6acfdec1777a788c466c1fd75bb1b
Tony Trinh <tonytrinh911@gmail.com>
2023-03-02 21:00:35 -0500

    added the template for the README and template literals for data input

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,39 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectTitle}
 
+  ## Licensing: 
+  [![license](https://img.shields.io/badge/license-${data.license}-green)](https://shields.io)
+
+  ##Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Description:
+  ${data.description}
+
+  ## Installation:
+  ${data.installation}
+
+  ## Usage:
+  ${data.usage}
+
+  ##License: 
+  ${data.license}
+
+  ## Contributing: 
+  ${data.contributing}
+
+  ##Tests
+  ${data.tests}
+
+  ## Questions
+  - Visit my GitHub: [${data.username}](https://github.com/${data.username})
+  - Email me with any additional questions: ${data.email}
 `;
 }
 
